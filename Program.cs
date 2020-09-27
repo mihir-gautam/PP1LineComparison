99c6ec6b70c03ba4da08513af91e4b9e8e99dbf6
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-09-28 00:01:50 +0530

    [Mihir] Check for Equal Lenght Use Case

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 
-namespace LineComparisonProblem
+namespace CheckEqualsCondition
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int x1, y1, x2, y2;
+            int x1, y1, x2, y2, x3, y3, x4, y4;
+
+            Console.WriteLine("Enter inputs for the first line");
             Console.WriteLine("Input the X1: ");
             x1 = Convert.ToInt32(Console.ReadLine());
 
@@ -19,8 +21,86 @@ namespace LineComparisonProblem
             Console.WriteLine("Input the Y2: ");
             y2 = Convert.ToInt32(Console.ReadLine());
 
-            double lineLength = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
-            Console.WriteLine("Length of a line = "+ lineLength);
+            double lineLength1 = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
+            Console.WriteLine("Length of the first line = " + lineLength1);
+
+            Console.WriteLine("Enter inputs for the second line");
+            Console.WriteLine("Input the X1: ");
+            x3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the Y1: ");
+            y3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the X2: ");
+            x4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the Y2: ");
+            y4 = Convert.ToInt32(Console.ReadLine());
+
+            double lineLength2 = Math.Sqrt(Math.Pow((x3 - x4), 2) + Math.Pow((y3 - y4), 2));
+            Console.WriteLine("Length of the second line = " + lineLength2);
+
+            if (lineLength1 == lineLength2)
+            {
+                Console.WriteLine("Both lines are equal in length");
+            }
+            else
+            {
+                Console.WriteLine("Lines are Not Equal in length");
+            }
+        }
+    }
+}
+using System;
+
+namespace CheckEqualsCondition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1, y1, x2, y2, x3, y3, x4, y4;
+
+            Console.WriteLine("Enter inputs for the first line");
+            Console.WriteLine("Input the X1: ");
+            x1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the Y1: ");
+            y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the X2: ");
+            x2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the Y2: ");
+            y2 = Convert.ToInt32(Console.ReadLine());
+
+            double lineLength1 = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
+            Console.WriteLine("Length of the first line = " + lineLength1);
+
+            Console.WriteLine("Enter inputs for the second line");
+            Console.WriteLine("Input the X1: ");
+            x3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the Y1: ");
+            y3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the X2: ");
+            x4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the Y2: ");
+            y4 = Convert.ToInt32(Console.ReadLine());
+
+            double lineLength2 = Math.Sqrt(Math.Pow((x3 - x4), 2) + Math.Pow((y3 - y4), 2));
+            Console.WriteLine("Length of the second line = " + lineLength2);
+
+            if (lineLength1 == lineLength2)
+            {
+                Console.WriteLine("Both lines are equal in length");
+            }
+            else
+            {
+                Console.WriteLine("Lines are Not Equal in length");
+            }
         }
     }
 }
