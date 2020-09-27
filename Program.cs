667a738ae0d751409a04ca47b30d7bb4ec3f9e55
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-09-27 23:32:37 +0530

    [Mihir] Add Line of Length Function

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LineComparisonProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1, y1, x2, y2;
+            Console.WriteLine("Input the X1: ");
+            x1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the Y1: ");
+            y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the X2: ");
+            x2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the Y2: ");
+            y2 = Convert.ToInt32(Console.ReadLine());
+
+            double lineLength = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
+            Console.WriteLine("Length of a line = "+ lineLength);
+        }
+    }
+}
